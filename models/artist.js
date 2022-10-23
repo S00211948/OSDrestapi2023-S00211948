814c98546c15288336faaab5ef764d71516942dc
shanegillespie <S00211948@atu.ie>
2022-10-23 21:18:53 +0100

    Added route to retrieve all songs

--- models/artist.js
+++ models/artist.js
@@ -30,27 +30,28 @@ const artistSchema = new mongoose.Schema(
 function ValidateArtist(artist)
 {
     const songJoiSchema = Joi.object({
-        title: Joi.string().min(3).required().messages({"any.required" : "Title1 field is required"}),
+        title: Joi.string().min(3).required().messages({"any.required" : "Song Title field is required"}),
         artist: Joi.string(),
         release_year: Joi.number().integer().min(1600).messages({"number.base" : "Year value must be a number"}),
     })
 
     const albumJoiSchema = Joi.object({
-        title: Joi.string().min(3).required().messages({"any.required" : "Title2 field is required"}),
+        title: Joi.string().min(3).required().messages({"any.required" : "Album Title field is required"}),
         artist: Joi.string(),
-        release_year: Joi.number().integer().min(1600).messages({"number.base" : "Year value must be a number"}),
+        release_year: Joi.number().integer().min(1600).messages({"number.base" : "Year value must be a number - min 1600"}),
         songs: Joi.array().items(songJoiSchema)
     })
 
     const artistJoiSchema = Joi.object({
         name: Joi.string().min(2).required().messages({"any.required" : "artist name is a required field"}),
         nationality: Joi.string(),
-        year_formed: Joi.number().integer().min(1600),
+        year_formed: Joi.number().integer().min(1600).messages({"number.base" : "year formed must be a number - min 1600"}),
         albums: Joi.array().items(albumJoiSchema)
     })
     return artistJoiSchema.validate(artist);
 }
 
 const Artist = mongoose.model('artist', artistSchema);
+const Song = mongoose.model('song', songSchema);
 
-module.exports = {Artist, ValidateArtist}
+module.exports = {Artist, ValidateArtist, Song}
--- routes/songs.js
+++ routes/songs.js
@@ -1,142 +1,26 @@
 const express = require('express');
-const {Artist, ValidateArtist} = require('../models/artist');
+const {Artist, Song, ValidateArtist} = require('../models/artist');
 
 const router = express.Router();
 
    router.get('/', async (req, res) => {
   
-    try {
-      const { title, release_year, limit, pagenumber, pagesize } = req.query;
-      let filter = {};
-  
-      if (title) {
-        filter.title = { $regex: `${title}`, $options: 'i' }
-      }
+    try {      
+      var songs = [];
+      const artists = await Artist.find().select('albums');
 
-      const yearNumber = parseInt(release_year)
-
-      if (!isNaN(yearNumber) ) {
-        Number.isInteger(release_year)
-          filter.release_year = yearNumber
-      }
-
-      let limitNumber = parseInt(limit)
-      if (isNaN(limitNumber)) {
-        limitNumber = 0
-      }
-
-      let pageSizeNumber = parseInt(pagesize);
-
-      if (isNaN(pageSizeNumber)) {
-        pageSizeNumber = 0
-      }
-      let pageNumberNumber = parseInt(pagenumber);
-    
-      if (isNaN(pageNumberNumber)) {
-        pageNumberNumber = 1
-      }
-    
-      console.table([{Title:filter.title, Year:filter.release_year}]);    
-
-      const artists = await Artist
-      .find(filter)
-      .limit(limitNumber)
-      .sort({release_year:1})
-      .skip((pageNumberNumber -1)*pageSizeNumber)
-      .select('title release_year');
-      res.json(artists);
+      artists.forEach(album => {
+        var subDocs = album.$getAllSubdocs()
+        subDocs.forEach(song => {
+          songs += song
+        });
+      });
+      res.json(songs);
     }
     catch (error){
       console.log(error);
       res.status(500).json('db error')
     }
   })
-  
-  router.get('/:id',async (req,res) => {
-     let id = req.params.id;
-  
-     try
-     {
-      const artist = await Artist.findById(req.params.id);
-      if(artist)
-      {
-        res.json(artist);
-      }
-      else
-      {
-        res.status(404).json('Not found')
-      }
-     }
-     catch (error)
-     {
-      res.status(404).json('Not found: id is weird ' + error);
-     }
-  })
-  
-  
-  router.post('/', async (req, res) => {
 
-    let result = ValidateArtist(req.body);
-    if(result.error)
-    {
-      res.status(400).json(result.error);
-      return;
-    }
-
-    let artist = new Artist(req.body);
-
-    try
-    {
-      artist = await artist.save();
-      res
-      .location(`${artist._id}`)
-      .status(201)
-      .json(artist)
-    }
-    catch (error)
-    {
-      res.status(500).send('db_error ' + error)
-    }
-  })
-  
-  
-  router.delete('/:id', async(req, res) =>
-   {
-    try 
-    {
-      const artist = await Artist.findByIdAndDelete(req.params.id);
-      if (artist)
-        res.status(204).send();
-      else
-        res.status(404).json(`artist with that ID ${req.params.id} was not found`)
-    }
-    catch 
-    {
-      res.status(404).json(`funny id ${req.params.id} was not found`);
-    }
-  
-    })
-
-    router.put('/:id', async(req,res) =>
-    {
-      let result = ValidateArtist(req.body);
-      if(result.error)
-      {
-        res.status(400).json(result.error);
-        return;
-      }
-      try
-      {
-        const artist = await Artist.findByIdAndUpdate(req.params.id, req.body);
-        if(artist)
-          res.status(204).json(artist);
-        else
-          res.status(404).json(`artist with that ID ${req.params.id} was not found`)
-      }
-      catch
-      {
-        res.status(404).json(`funny id ${req.params.id} was not found`);
-      }
-    })
-
-    module.exports = router
+  module.exports = router
